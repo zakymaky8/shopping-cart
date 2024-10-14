2d394520afd01390e74a7cb262be0e1344d339bf
Gizachew Meku <123456789+odin@zakymak.noreply.github.com>
2024-10-15 02:38:56 +0300

    set up the minimum possible requirement of the starter project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shopping-cart
+
 Classic Mock Shopping cart built using React + Vite and packaged with fetching, routing, testing, css module styling and some other ingredients
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <Fragment>
+      <h1>Hello Vite</h1>
+    </Fragment>
+  )
+}
+
+export default App
