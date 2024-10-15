b21c92e6327c721f3f9f4e75b857e1a03740d556
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-10-15 03:19:13 +0300

    check if any  modular css is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Fragment } from 'react'
 import './App.css'
+import someStyle from "./App.module.css"
 
 function App() {
 
   return (
     <Fragment>
-      <h1>Hello Vite</h1>
+      <div className={someStyle.someStyle}>
+          
+        <h1>Hello Vite</h1>
+
+      </div>
     </Fragment>
   )
 }
