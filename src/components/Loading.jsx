4ad35eb86e4d9e8b036672e4fd3bf406e254a55f
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-10-19 13:42:42 +0300

    finish project and build

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import styles from "../styles/Errors.module.css"
 
 export default function Loading() {
   return (
-    <div>
+    <div className={styles.loadCont}>
       <div className={styles.loadingSpinner}></div>
     </div>
   )
